8eb7804f26a6cf4919eb6f300a63b5bd5f29f14b
Hershal Bhave <hershal.bhave@gmail.com>
2016-10-26 14:11:10 -0500

    add tx, rx

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ whoshome.then(function (people) {
 
 
 function printTable(people) {
-  const wifiHeader = ['hostname', 'ip', 'uptime', 'quality'];
+  const wifiHeader = ['hostname', 'ip', 'uptime', 'tx', 'rx', 'quality'];
 
   let wifiPeople = _(people)
-          .filter((p) => typeof p.wifi != 'undefined')
-          .map((person) =>
-               _.map(['hostname', 'ip', 'wifi.uptime', 'wifi.quality'], (keyPath) => _.get(person, keyPath)))
-          .value();
+      .filter((p) => typeof p.wifi != 'undefined')
+      .map((person) =>
+           _.map(['hostname', 'ip', 'wifi.uptime', 'wifi.tx', 'wifi.rx', 'wifi.quality'], (keyPath) => _.get(person, keyPath)))
+      .value();
 
   let wiredPeople = _(people)
-          .filter((p) => typeof p.wifi == 'undefined')
-          .map((person) =>
-               _.map(['hostname', 'ip'], (keyPath) => _.get(person, keyPath)))
-          .map((arr) => _.concat(arr, ['', '']))
-          .value();
+      .filter((p) => typeof p.wifi == 'undefined')
+      .map((person) =>
+           _.map(['hostname', 'ip'], (keyPath) => _.get(person, keyPath)))
+      .map((arr) => _.concat(arr, ['', '', '', '']))
+      .value();
 
   let table = new cliTable({head: wifiHeader, chars: {'mid': '', 'left-mid': '', 'mid-mid': '', 'right-mid': ''}});
   wifiPeople.forEach((p) => table.push(p));
